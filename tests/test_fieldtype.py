e85cae3c4754d8ab7eeaa8732be5c6f48bb9a240
Lelzin λ <d3cryptofc@gmail.com>
2024-11-05 00:24:27 -0300

    test: add unit tests for `FieldType` class
    - test_non_integer_size
    - test_invalid_integer_size

---
+++ tests/test_fieldtype.py
@@ -0,0 +1,17 @@
+import pytest
+
+from structer.fieldtype import FieldType
+
+
+def test_non_integer_size():
+    given = None
+    expected = TypeError
+    with pytest.raises(expected):
+        FieldType(given)
+
+
+@pytest.mark.parametrize('given', (0, -1))
+def test_invalid_integer_size(given):
+    expected = ValueError
+    with pytest.raises(expected):
+        FieldType(given)
