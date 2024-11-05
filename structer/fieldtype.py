fc3d2cf0b40fadc5ec2786560e8d9c58beba37e7
Lelzin λ <d3cryptofc@gmail.com>
2024-11-05 00:53:59 -0300

    feat: add `FieldTypeABC` abstract class
    abstract properties:
    
    - size(self): to get field type size
    
    abstract methods:
    
    - _validate_encode(self, value: str) -> None
    - _validate_decode(self, value: Union[bytes, bytearray]) -> None
    - encode(self, value: str) -> bytes
    - decode(self, value: Union[bytes, bytearray], **kwargs) -> str

--- structer/fieldtype.py
+++ structer/fieldtype.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Union
+
 class FieldType:
     def __init__(self, size: int):
         if not isinstance(size, int):
@@ -8,3 +13,21 @@ class FieldType:
 
     def __repr__(self):
         return '{}({!r})'.format(type(self).__name__, self._size)
+
+
+class FieldTypeABC(ABC):
+    @abstractproperty
+    def size(self): ...
+
+    @abstractmethod
+    def _validate_encode(self, value: str) -> None: ...
+
+    @abstractmethod
+    def encode(self, value: str) -> bytes: ...
+
+    @abstractmethod
+    def decode(self, value: Union[bytes, bytearray], **kwargs) -> str: ...
+
+    @abstractmethod
+    def _validate_decode(self, value: Union[bytes, bytearray]) -> None: ...
+
